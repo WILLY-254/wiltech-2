2fc52d4620e8edb4783bf1d84eb6fd64f34d0ff1
WILLY 254 <kaiyongiwilfred90@gmil.com>
2024-11-07 10:59:34 +0300

    comm

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";  // Show current slide
   setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
-
+// lll
 document.addEventListener("DOMContentLoaded", showSlides); // Initialize slideshow on load
